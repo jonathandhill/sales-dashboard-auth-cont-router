3c955ced9cb3f49806956b02027bb3c1a813a3f4
Jonathan Hill <144107180+MrHighViolet@users.noreply.github.com>
2025-04-26 18:07:25 +0100

    tested with context, router next

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { supabase } from "../supabaseClient";
+import supabase from "../supabase-client";
 
 const AuthContext = createContext();
 
